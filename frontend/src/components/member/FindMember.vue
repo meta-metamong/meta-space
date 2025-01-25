7c399a773646dbfb5c787a172a8512645c187b17
1soo <aaa4799@naver.com>
2025-01-25 16:35:25 +0900

    style: 들여쓰기 공간 변경

--- frontend/src/components/member/FindMember.vue
+++ frontend/src/components/member/FindMember.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="container mt-5 d-flex justify-content-center">
-      <!-- 아이디 찾기 -->
-      <div class="card p-4 mx-3" style="width: 400px;">
-        <h3 class="text-center mb-4">{{ title }} 찾기</h3>
-        <form @submit.prevent="handleSubmit">
-            <div class="mb-3" v-if="idOrPw === 'pw'">
-                <input type="text" class="form-control" placeholder="아이디" name="id" v-model="id" required />
-            </div>
-            <div class="mb-3">
-                <input type="text" class="form-control" placeholder="이름" name="name" v-model="name" required />
-            </div>
-            <div class="mb-3">
-                <input type="email" class="form-control" placeholder="이메일" name="email" v-model="email" required />
-            </div>
-                <input type="hidden" name="idOrPw" v-bind:value="idOrPw" />
+        <!-- 아이디 찾기 -->
+        <div class="card p-4 mx-3" style="width: 400px;">
+            <h3 class="text-center mb-4">{{ title }} 찾기</h3>
+            <form @submit.prevent="handleSubmit">
+                <div class="mb-3" v-if="idOrPw === 'pw'">
+                    <input type="text" class="form-control" placeholder="아이디" name="id" v-model="id" required />
+                </div>
+                <div class="mb-3">
+                    <input type="text" class="form-control" placeholder="이름" name="name" v-model="name" required />
+                </div>
+                <div class="mb-3">
+                    <input type="email" class="form-control" placeholder="이메일" name="email" v-model="email" required />
+                </div>
+                    <input type="hidden" name="idOrPw" v-bind:value="idOrPw" />
                 <button type="submit" class="btn btn-primary w-100">{{ title }} 찾기</button>
-        </form>
-      </div>
+            </form>
+        </div>
     </div>  
 </template>
 <script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,14 +3,14 @@ import FindIdPw from '../pages/FindIdPw.vue';
 import Home from '../pages/Home.vue';
 
 
- const routes = [
-   { path: '/', component: Home },
-   { path: '/find', component: FindIdPw }
- ];
+const routes = [
+	{ path: '/', component: Home },
+	{ path: '/find', component: FindIdPw }
+];
 
- const router = createRouter({
-    history: createWebHistory(), 
-    routes
- });
+const router = createRouter({
+	history: createWebHistory(), 
+	routes
+});
  
  export default router;
