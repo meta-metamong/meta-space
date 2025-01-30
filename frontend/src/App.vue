720eccd07028eb0a4b7f3659e577b71901194667
Lee Yeon Su <yslee0540@gmail.com>
2025-01-30 21:44:35 +0900

    feat: Footer 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,16 +3,19 @@
         <Header />
 		<hr />
 		<router-view />
+		<Footer />
 	</div>
 </template>
 
 <script>
 import Header from "./components/common/Header.vue";
+import Footer from "./components/common/Footer.vue";
 
 export default {
 	name: "App",
     components: {
-        Header
+        Header,
+		Footer
     }
 }
 </script>
