8c181d797723ec8ae5ca18753b7a3911583ace47
pgd <rudeh1253@gmail.com>
2025-02-06 10:33:41 +0900

    feat: define entity for facility image with inheritance

---
+++ backend/src/main/java/com/metamong/mt/global/image/model/Image.java
@@ -0,0 +1,49 @@
+package com.metamong.mt.global.image.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "image")
+@SequenceGenerator(
+        name = "img_pk_generator",
+        sequenceName = "img_pk_seq",
+        initialValue = 1,
+        allocationSize = 1
+)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "img_attatched_to")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "img_pk_generator")
+    private Long imgId;
+    
+    private String imgPath;
+    private Integer imgDisplayOrder;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt = LocalDateTime.now();
+    
+    public Image(Long imgId) {
+        this.imgId = imgId;
+    }
+    
+    protected Image(String imgPath, Integer imgDisplayOrder) {
+        this.imgPath = imgPath;
+        this.imgDisplayOrder = imgDisplayOrder;
+    }
+}
