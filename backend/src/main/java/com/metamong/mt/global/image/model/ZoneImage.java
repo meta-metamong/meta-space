a1e3bdc6569d4e80035e41f29834d4a928133d69
pgd <rudeh1253@gmail.com>
2025-02-06 12:13:09 +0900

    feat: define ZoneImage entity

---
+++ backend/src/main/java/com/metamong/mt/global/image/model/ZoneImage.java
@@ -0,0 +1,28 @@
+package com.metamong.mt.global.image.model;
+
+import com.metamong.mt.domain.facility.model.Zone;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@DiscriminatorValue("Z")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ZoneImage extends Image {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "zone_id")
+    private Zone zone;
+    
+    public ZoneImage(String imgPath, Integer imgDisplayOrder, Zone zone) {
+        super(imgPath, imgDisplayOrder);
+        this.zone = zone;
+    }
+}
