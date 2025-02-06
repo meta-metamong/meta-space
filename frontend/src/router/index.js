958a4c8d54206e7dc54e60ec40ff2bbab3456ea4
1soo <aaa4799@naver.com>
2025-02-06 18:31:13 +0900

    style: router/index.js 주석 해제

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -13,18 +13,17 @@ import DetailReservation from '../components/reservation/DetailReservation.vue';
 const routes = [
 	{ path: '/', component: Home },
 	{ path: '/login', component: Login },
-	
 	{ path: '/mypage', component: MyPage },
 	{ path: '/update', component: UpdateMember },
 	{ path: '/signup', component: SignUp},
-	{ path: '/download', component: Download  },
-	
 	//{ path: '/socket', component: Socket  },
-	//{ path: '/admin', component: Admin },
-	//{ path: '/admin/socket', component: ChatAdmin },
-	//{ path: '/admin/chatlist', component: ChatList },
+	{ path: '/download', component: Download  },
 
-	{ path: '/reservation', component: Reservation},
+	{ path: '/admin', component: Admin },
+	{ path: '/admin/chatlist', component: ChatList },
+
+	{ path: '/reservation', component: Reservation },
+	{ path: '/reservation/:id', component: DetailReservation, props: true },
 ];
 
 const router = createRouter({
