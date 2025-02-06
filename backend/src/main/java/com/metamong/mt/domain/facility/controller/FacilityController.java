df0e6420542dc231bfc12aa8cacfa96e55966d75
pgd <rudeh1253@gmail.com>
2025-02-06 11:59:33 +0900

    feat: facility api endpoint for post facility

---
+++ backend/src/main/java/com/metamong/mt/domain/facility/controller/FacilityController.java
@@ -0,0 +1,29 @@
+package com.metamong.mt.domain.facility.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.metamong.mt.domain.facility.dto.request.FacilityRegistrationRequestDto;
+import com.metamong.mt.domain.facility.dto.response.FacilityRegistrationResponseDto;
+import com.metamong.mt.domain.facility.service.FacilityService;
+import com.metamong.mt.global.apispec.BaseResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class FacilityController {
+    private final FacilityService facilityService;
+    
+    @PostMapping("/facilities")
+    public ResponseEntity<BaseResponse<FacilityRegistrationResponseDto>> postFacility(
+            @RequestBody FacilityRegistrationRequestDto requestBody) {
+        return ResponseEntity.status(HttpStatus.CREATED.value())
+                .body(BaseResponse.of(this.facilityService.registerFacility(requestBody), HttpStatus.CREATED));
+    }
+}
