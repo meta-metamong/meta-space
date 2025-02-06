f0740f49e9b38634223b8c149b7834196ed56c34
pgd <rudeh1253@gmail.com>
2025-02-06 12:29:44 +0900

    feat: implement save zone image

--- backend/src/main/java/com/metamong/mt/domain/facility/repository/mybatis/FacilityMapper.java
+++ backend/src/main/java/com/metamong/mt/domain/facility/repository/mybatis/FacilityMapper.java
@@ -5,13 +5,10 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.metamong.mt.domain.facility.model.AdditionalInfo;
-import com.metamong.mt.domain.facility.model.Zone;
 
 @Repository
 @Mapper
 public interface FacilityMapper {
-
-    void saveZone(@Param("zone") Zone zone);
     
     void saveAdditionalInfo(@Param("addinfo") AdditionalInfo addinfo);
 }
