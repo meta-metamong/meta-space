a899cd28df30c44cc15beb2440a87a7a59308ef1
pgd <rudeh1253@gmail.com>
2025-02-06 10:21:07 +0900

    feat: define entities related to facility domain

---
+++ backend/src/main/java/com/metamong/mt/domain/facility/model/Category.java
@@ -0,0 +1,61 @@
+package com.metamong.mt.domain.facility.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@SequenceGenerator(
+        name = "cat_pk_generator",
+        sequenceName = "cat_pk_seq",
+        initialValue = 1,
+        allocationSize = 1
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cat_pk_generator")
+    private Long catId;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_cat_id")
+    private Category parentCat;
+    
+    private String catName;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "parentCat")
+    private List<Category> children = new ArrayList<>();
+    
+    public Category(Long catId) {
+        this.catId = catId;
+    }
+    
+    public Category(String catName) {
+        this.catName = catName;
+    }
+    
+    public Category(Category parentCat, String catName) {
+        this.parentCat = parentCat;
+        this.catName = catName;
+    }
+    
+    public void addChildCategory(Category childCategory) {
+        this.children.add(childCategory);
+    }
+}
