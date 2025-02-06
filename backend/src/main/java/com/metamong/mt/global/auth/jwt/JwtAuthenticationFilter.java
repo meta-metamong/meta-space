7c341fc4e05cfd73213ed9fe06b8197a5ad91f4e
pgd <rudeh1253@gmail.com>
2025-02-06 17:44:21 +0900

    refactor: not register JwtAuthenticationFilter to Spring Bean

--- backend/src/main/java/com/metamong/mt/global/auth/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/metamong/mt/global/auth/jwt/JwtAuthenticationFilter.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import com.metamong.mt.global.config.constant.HttpRequestAuthorizationDefinition;
@@ -22,7 +21,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
-@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
