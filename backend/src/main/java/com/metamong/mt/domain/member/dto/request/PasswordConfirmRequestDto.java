ecf5c111c7fa0af1325945995988b6f6a214173e
1soo <aaa4799@naver.com>
2025-02-07 20:40:36 +0900

    feat: 비밀번호 확인

--- backend/src/main/java/com/metamong/mt/domain/member/dto/request/PasswordConfirmRequestDto.java
+++ backend/src/main/java/com/metamong/mt/domain/member/dto/request/PasswordConfirmRequestDto.java
@@ -1,5 +1,6 @@
 package com.metamong.mt.domain.member.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,5 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PasswordConfirmRequestDto {
+    
+    @NotEmpty(message="비밀번호는 입력되어야 합니다.")
     private String password;
 }
