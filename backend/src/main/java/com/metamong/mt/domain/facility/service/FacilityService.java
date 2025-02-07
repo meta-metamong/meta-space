2b05fb59ea88df35b01829611c34a00c14d62a87
pgd <rudeh1253@gmail.com>
2025-02-07 11:03:37 +0900

    feat: implement API endpoint to update facility

--- backend/src/main/java/com/metamong/mt/domain/facility/service/FacilityService.java
+++ backend/src/main/java/com/metamong/mt/domain/facility/service/FacilityService.java
@@ -1,12 +1,16 @@
 package com.metamong.mt.domain.facility.service;
 
 import com.metamong.mt.domain.facility.dto.request.FacilityRegistrationRequestDto;
+import com.metamong.mt.domain.facility.dto.request.FacilityUpdateRequestDto;
 import com.metamong.mt.domain.facility.dto.response.FacilityRegistrationResponseDto;
 import com.metamong.mt.domain.facility.dto.response.FacilityResponseDto;
+import com.metamong.mt.domain.facility.dto.response.FacilityUpdateResponseDto;
 
 public interface FacilityService {
 
     FacilityRegistrationResponseDto registerFacility(FacilityRegistrationRequestDto dto);
     
     FacilityResponseDto getFacility(Long facilityId);
+    
+    FacilityUpdateResponseDto updateFacility(Long fctId, FacilityUpdateRequestDto dto);
 }
