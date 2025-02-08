8386554b16acf1f396869287a17614853e334ace
hyelin <777gpfls>
2025-02-08 18:18:02 +0900

    admin

--- backend/src/main/java/com/metamong/mt/domain/admin/repository/mybatis/AdminMapper.java
+++ backend/src/main/java/com/metamong/mt/domain/admin/repository/mybatis/AdminMapper.java
@@ -1,10 +1,12 @@
 package com.metamong.mt.domain.admin.repository.mybatis;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import com.metamong.mt.domain.admin.dto.response.FacilitySearchResponseDto;
 import com.metamong.mt.domain.admin.dto.response.MemberSearchResponseDto;
 import com.metamong.mt.domain.admin.dto.response.ReportedMemberResponseDto;
 
@@ -12,6 +14,18 @@ import com.metamong.mt.domain.admin.dto.response.ReportedMemberResponseDto;
 @Mapper
 public interface AdminMapper {
 	List<MemberSearchResponseDto> searchMembers();
-	
+
 	List<ReportedMemberResponseDto> getReportedMembers();
+
+    void updateFacilityStateRegApproved(Map<String, Object> params);
+
+    void insertNotification(Map<String, Object> params);
+
+    void updateFacilityStateRegRejected(Map<String, Object> params);
+
+    void updateFacilityStateDelApproved(Map<String, Object> params);
+
+    void updateFacilityStateDelRejected(Map<String, Object> params);
+
+    List<FacilitySearchResponseDto> searchFacilities();
 }
