f028d9299b98636eca962c40bb1f563da9b75fe6
hyelin <777gpfls>
2025-02-10 20:40:41 +0900

    대시보드 수정정

--- frontend/src/pages/AdminHome.vue
+++ frontend/src/pages/AdminHome.vue
@@ -36,45 +36,53 @@
   
       // Highcharts로 라인 차트 그리기
       drawChart() {
-        // 각 시설별 예약 데이터를 시리즈로 나누기
-        const seriesData = this.reservationData.map((reservation) => {
-          return {
-            name: reservation.fctId, // 시설명
-            data: [
-              reservation.mon,    // 월요일
-              reservation.tues,   // 화요일
-              reservation.wednes, // 수요일
-              reservation.thurs,  // 목요일
-              reservation.fri,    // 금요일
-              reservation.satur,   // 토요일
-              reservation.sun,    // 일요일
-            ]
-          };
-        });
-  
-        // Highcharts 라인 차트 설정
-        Highcharts.chart("highchart-container", {
-          title: {
-            text: "이번 주 시설별 예약 현황"
-          },
-          xAxis: {
-            categories: this.daysOfWeek // 요일
-          },
-          yAxis: {
-            title: {
-              text: "예약 건수"
-            },
-            tickInterval: 1,  // Y축을 1씩 증가하도록 설정
-          },
-          series: seriesData, // 시설별 예약 건수 데이터
-          exporting: {
-            enabled: true, // 내보내기 활성화
-          },
-          accessibility: {
-            enabled: false, // 접근성 기능 비활성화
-          }
-        });
-      }
+  // 각 시설별 예약 데이터를 시리즈로 나누기
+  const seriesData = this.reservationData.map((reservation) => {
+    return {
+      name: reservation.fctId, // 시설명
+      data: [
+        reservation.mon,    // 월요일
+        reservation.tues,   // 화요일
+        reservation.wednes, // 수요일
+        reservation.thurs,  // 목요일
+        reservation.fri,    // 금요일
+        reservation.satur,  // 토요일
+        reservation.sun,    // 일요일
+      ]
+    };
+  });
+
+  // Highcharts 라인 차트 설정
+  Highcharts.chart("highchart-container", {
+    title: {
+      text: "이번 주 시설별 예약 현황"
+    },
+    xAxis: {
+      categories: this.daysOfWeek, // 요일
+    },
+    yAxis: {
+      lineWidth: 1,
+  tickWidth: 1,
+  title: {
+    align: 'high',
+    offset: 0,
+    text: '예약수 (건)',
+    rotation: 0,
+    y: -10
+  },
+  tickInterval: 1  // Y축의 간격을 1로 설정ㄴ
+    },
+    series: seriesData, // 시설별 예약 건수 데이터
+    exporting: {
+      enabled: true, // 내보내기 활성화
+    },
+    accessibility: {
+      enabled: false, // 접근성 기능 비활성화
+    },
+  });
+}
+
+
     },
     mounted() {
       this.fetchData();  // 컴포넌트가 마운트될 때 데이터 가져오기
