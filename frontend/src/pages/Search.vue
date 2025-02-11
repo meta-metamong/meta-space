5636440b68ebac0183f0c47cea5aa9c35501ca6e
1soo <aaa4799@naver.com>
2025-02-11 16:43:39 +0900

    feat: 인기검색어

--- frontend/src/pages/Search.vue
+++ frontend/src/pages/Search.vue
@@ -16,19 +16,36 @@
 
 <script>
 import SearchBox from '../components/common/SearchBox.vue';
+import { get } from "../apis/axios";
 export default{
     name: 'Search',
     data(){
         return{
-            popularKeywords: []
+            popularKeywords: [],
         }
     },
     components:{
         SearchBox
     },
-    mounted(){
-        for(let i = 0; i < 10; i++){
-            this.popularKeywords.push(`인기검색어`);
+    async mounted(){
+        const response = await get("/search/popular");
+        if(response.status === 200){
+            this.popularKeywords = response.data.content.popularKeywords;
+            if(this.popularKeywords.length === 0){
+                this.popularKeywords.push("수영");
+                this.popularKeywords.push("골프");
+                this.popularKeywords.push("축구");
+                this.popularKeywords.push("테니스");
+                this.popularKeywords.push("배드민턴");
+                this.popularKeywords.push("운동장");
+                this.popularKeywords.push("대강당");
+                this.popularKeywords.push("헬스");
+                this.popularKeywords.push("야구");
+                this.popularKeywords.push("필라테스");
+            }
+        }else{
+            alert("인기 검색어 조회가 실패됐습니다.");
+            return;
         }
     }
 }
